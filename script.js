db53c55df23eb2fab219bdd57ee85447bbecdee3
rafael begnis da silva <rafaelbegnis123@gmail.com>
2025-09-09 22:28:53 -0300

    implementacao da barra de busca

--- script.js
+++ script.js
@@ -2,12 +2,44 @@ $(document).ready(function () {
   $('.pergunta').click(function () {
     let elpergunta = $(this);
     let elresposta = elpergunta.next('.resposta');
+
     $('.resposta').not(elresposta).slideUp(200);
+
     elresposta.slideToggle();
 
-  })
+    $('.pergunta').not(elpergunta)
+      .removeClass('ativa')
+      .find('.icone')
+      .html("+");
+
+    elpergunta.toggleClass('ativa');
+
+    let icone = elpergunta.find('.icone');
+
+    if (elpergunta.hasClass('ativa')) {
+      icone.html("-");
+    } else {
+      icone.html("+");
+    }
+  });
+
+  $('#busca-faq').on('keyup', function () {
+    let termoBusca = $(this).val().toLowerCase();
+
+    $('.faq-item').each(function () {
+      let textoItem = $(this).text().toLowerCase();
+      
+      if (textoItem.includes(termoBusca)) {
+        $(this).show();
+
+      } else {
+        $(this).hide();
+      }
+
+    })
 
 
+  });
 
 });
 
